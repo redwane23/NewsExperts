eaaea59f40bf1bce1c6b6f4b58c9642bfa2bb9e6
redwane23 <redoianekassouse@gmail.com>
2025-06-14 23:15:54 +0100

    Update settings.py
    renaming the static root to static instead fo staticfiles

--- news/settings.py
+++ news/settings.py
@@ -117,7 +117,7 @@ LOGIN_URL = '/profile/login/'
 
 if not DEBUG:
 
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
